c950b63473487b8424d62755c8eb6e729d04c4ac
RichA <rich_ay@sbcglobal.net>
2021-01-20 21:26:56 -0600

    Have main carosel working.:

--- client/src/components/Properties/index.js
+++ client/src/components/Properties/index.js
@@ -1,5 +1,5 @@
 import React, { useState, Component } from "react";
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
@@ -7,45 +7,73 @@ import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.
 
 
 
-function Properties() {
+// function Properties() {
 
-  const [properties] = useState([
-    {
-      id: 1,
-      name: 'Home1',
-      description: '',
-    },
-    {
-      id: 2,
-      name: 'Home1',
-      description: '',
-    },
-    {
-      id: 3,
-      name: 'Home1',
-      description: '',
-    },
-  ])
+
 
   // handleClick  => {
 
   // }
 
-  return (
-    <Carousel>
-    <div>
-      {properties.map((image) => (
-        <img key={image.id} 
-          alt={image.name}
-          // width="300" height="auto" 
-          src={require(`../../assets/images/properties/${image.id}.jpg`)}
-          // onClick={handleClick}
-          >
-        </img>
-      ))}
-    </div>
-    </Carousel>
-  );
-}
+  // return (
+  //   <Carousel>
+  //   <div>
+  //     {properties.map((image) => (
+  //       <img key={image.id} 
+  //         alt={image.name}
+  //         // width="300" height="auto" 
+  //         src={require(`../../assets/images/properties/${image.id}.jpg`)}
+  //         // onClick={handleClick}
+  //         >
+  //       </img>
+  //     ))}
+  //   </div>
+  //   </Carousel>
+  // );
+
+  class Properties extends Component {
+    
+    render() {
+
+      // const [properties] = useState([
+      //   {
+      //     id: 1,
+      //     name: 'Home1',
+      //     description: '',
+      //   },
+      //   {
+      //     id: 2,
+      //     name: 'Home1',
+      //     description: '',
+      //   },
+      //   {
+      //     id: 3,
+      //     name: 'Home1',
+      //     description: '',
+      //   },
+      // ])
+
+         return (
+          <Carousel>
+          <div>
+              <img src={require("../../assets/images/properties/1.jpg" )}/>
+              <p className="legend">Home 1</p>
+         </div>
+         <div>
+              <img src={require("../../assets/images/properties/2.jpg" )} />
+              <p className="legend">Home 2</p>
+          </div>
+          <div>
+              <img src={require("../../assets/images/properties/3.jpg" )} />
+              <p className="legend">Home 3</p>
+          </div>
+         </Carousel>
+       );
+    }
+ };
+
+//  ReactDOM.render(<Properties />);
+//  // Don't for
+
 
 export default Properties;
