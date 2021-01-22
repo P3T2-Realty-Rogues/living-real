3374c7e4ee0ac51ab460410fc8adc4fdb05e4d8e
RichA <rich_ay@sbcglobal.net>
2021-01-22 12:40:30 -0600

    More work on styling the menu & responsiveness.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,25 +7,57 @@ function Nav() {
 
   function showNavigation() {
     return (
+     //Original idea
+      // <div>
+ 
+      //     <Header>
+      //       <Link to="/">Living Real</Link>
+      //     </Header>
+
+      //     <Menu widths={5}>
+      //     <Menu.Item>
+      //       <Link to="/AdminDash">Admin Dashboard </Link>
+      //     </Menu.Item>
+      //     <Menu.Item>
+      //       <Link to="/TenantDash">Tenant Dashboard </Link>
+      //     </Menu.Item>
+      //     <Menu.Item>
+      //       <Link to="/Login">Login </Link>
+      //     </Menu.Item>
+      //     <Menu.Item>
+      //       <Link to="/RequestInfo">Request Info </Link>
+      //     </Menu.Item> 
+      //   </Menu>
+      // </div>
+      
+      // Option 2
       <div>
-        <Menu widths={5}>
           <Header>
             <Link to="/">Living Real</Link>
           </Header>
-          <Menu.Item>
-            <Link to="/AdminDash">Admin Dashboard </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/TenantDash">Tenant Dashboard </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/Login">Login </Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/RequestInfo">Request Info </Link>
-          </Menu.Item>
-        </Menu>
+
+          <div class="ui four item tiny menu ">
+            <Link class="item" to="/AdminDash">Admin Dashboard </Link>
+            <Link class="item" to="/TenantDash">Tenant Dashboard </Link>
+            <Link class="item" to="/Login">Login </Link>
+            <Link class="item active" to="/RequestInfo">Request Info </Link>
+          </div>
       </div>
+            
+      // Option 3
+    //   <div class="ui menu">
+
+    //     <Link class="item" to="/">Living Real</Link>
+
+    //     <div class="right menu">
+    //       <div class="ui item">
+    //         <Link class="item" to="/AdminDash">Admin Dashboard </Link>
+    //         <Link class="item" to="/TenantDash">Tenant Dashboard </Link>
+    //         <Link class="item" to="/Login">Login </Link>
+    //         <Link class="item active" to="/RequestInfo">Request Info </Link>
+    //       </div>
+    //     </div>
+    //   </div>
     );
   }
 
