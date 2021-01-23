3b80494ee8ec54b48e03c5ae95c498e37af4538e
greyw <greywhitt@gmail.com>
2021-01-23 15:31:56 -0600

    added comments to models

--- server/models/Tenant.js
+++ server/models/Tenant.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
+//this is data linked to the User model that tells us if they are actively renting and what their lease date is
 const tenantSchema = new Schema ({
     leaseDate: {
         type: String,
